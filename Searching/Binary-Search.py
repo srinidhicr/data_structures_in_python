e04f7c67a1e32e04317ee27f8fa8cebde9fb088d
Srinidhi Rajendran <94728810+srinidhicr@users.noreply.github.com>
2023-07-05 00:09:09 +0530

    Update Binary-Search.py

--- Searching/Binary-Search.py
+++ Searching/Binary-Search.py
@@ -1,3 +1,11 @@
+"""
+LOGIC
+Divide the array into half and check whether target is in the portion - 0 to mid-1 or in the other portion - mid+1 to h
+If it is equal to the target return the index
+Perform this repeatedly
+If an element is not found -1 is returned
+"""
+
 def binarySearch(nums, target):
     l, h = 0, len(nums)-1
     while(l<=h):
@@ -21,3 +29,16 @@ for _ in range(n):
 target = int(input("Enter the element to search for - "))
 index = binarySearch(nums, target)
 print(index)
+
+"""
+TIME COMPLEXITY - 
+worst case - O(logN)
+best case - O(1) - elements are already sorted
+avg case - O(logN)
+SPACE COMPLEXITY - O(1)
+
+
+--SUITABLE FOR LARGE DATASETS
+
+
+"""
